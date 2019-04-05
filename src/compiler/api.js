8514d2b52893463e064ace2beaabb698ab160cbf
zero1five <zerodengyin@gmail.com>
2019-04-06 03:54:18 +0800

    feat: complete print logic | 完善输出函数逻辑

--- src/compiler/api.js
+++ src/compiler/api.js
@@ -1,9 +1,16 @@
 const api = {
-  print: () => {
-
-    // if (templete.match(/%s/g)[0].length !== params.length) {
-    //   throw `[Error]print: template and params length not match`
-    // }
+  print: (templete, ...params) => {
+    const regex = /%s/g;
+    if (!templete) {
+      throw `[Error]print: need template are you sure one params exits`
+    }
+    if (templete.match(regex)[0].length !== params.length) {
+      throw `[Error]print: template and params length not match`
+    }
+    while(params.length) {
+      templete = templete.replace(/%s/, params.shift());
+    }
+    console.log(templete);
   }  
 }
 
